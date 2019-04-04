4d98f2bad82e7ad8b396a08033502fc811e83cd9
Willem Homan <willem.homan@bigcommerce.com>
2019-04-04 12:39:00 +1100

    readme update

--- create-db.sql
+++ create-db.sql
@@ -2,5 +2,5 @@ DROP SCHEMA IF EXISTS `doctrine`;
 CREATE SCHEMA `doctrine`
 DEFAULT CHARACTER SET utf8mb4
 DEFAULT COLLATE utf8mb4_unicode_ci;
-CREATE USER 'root'@'%' identified by 'magic';
+-- CREATE USER 'root'@'%' identified by 'magic';
 GRANT ALL ON `doctrine`.* TO 'root'@'%';
